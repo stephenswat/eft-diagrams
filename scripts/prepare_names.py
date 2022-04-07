d415f6bc60af8a24b3088bd72f4a30eab29e456b
rahulgrit <rahul.balasubramanian@cern.ch>
2022-04-07 18:31:40 +0200

    update parnames

--- scripts/prepare_names.py
+++ scripts/prepare_names.py
@@ -6,18 +6,46 @@ parnames = json.load(open("data/parnames.json","r"))
 print(names.keys())
 print(parnames.keys())
 
+pair = [str(i)+str(j) for i in range(1,4) for j in range(1,4)]
+quad = [str(i)+str(j)+str(k)+str(l) for i in range(1,4) for j in range(1,4) for k in range(1,4) for l in range(1,4)]
+
 for case,pars in parnames.items():
-    dict0 = {}
-    print("case : {0} with number of parameters {1}".format(case,str(len(pars))))
-    print("\n"+case)
-    for par in pars:
-        found = False
-        for texpar in names.keys():
-           if texpar in par:
-               par = par.replace("Re","").replace("Im","")
-               dict0[par] = names[texpar].replace("\\e","\\epsilon")
-               found = True
-        if found == False: print(par)
-    
-    with open("out_"+case+".json","w") as f:
-         json.dump(dict0,f,indent=4)
+    if case == "topU3l":
+        dict0 = {}
+        print("case : {0} with number of parameters {1}".format(case,str(len(pars))))
+        print("\n"+case)
+        for par in list(set(pars)):
+            #print("searching for {0}\n".format(par))
+            found = False
+            for texpar in names.keys():
+               if texpar == par:
+                   par = par.replace("Re","").replace("Im","")
+                   dict0[par] = names[texpar].replace("\\e","\\epsilon").replace("\\s_","\\sigma_").replace("\\s^","\\sigma^")
+                   found = True
+                   print("{0} ----> {1}".format(par, dict0[par]))
+               elif par[len(par)-4:] in quad:
+
+#                   print("inside 4")
+                   if texpar == par[0:len(par)-4].replace("Re","").replace("Im",""):
+                      par = par.replace("Re","").replace("Im","")#.replace("dag","dagger").replace("\\s_","\\sigma_").replace("\\s^","\\sigma^")
+                      dict0[par] = "\\big("+ names[texpar].replace("\\e","\\epsilon").replace("=","\\big)_{"+par[len(par)-4:]+"} = \\Big(")+"\\Big)_{"+par[len(par)-4:]+"}".replace("dag","dagger").replace("\\s_","\\sigma_").replace("\\s^","\\sigma^")
+                      found = True
+                      print("{0} ----> {1}".format(par, dict0[par]))
+
+               elif par[len(par)-2:] in pair:
+                   #print("pair"+par[0:len(par)-2])
+                   if texpar == par[0:len(par)-2].replace("Re","").replace("Im",""):
+                      par = par.replace("Re","").replace("Im","")#.replace("dag","dagger").replace("\\s_","\\sigma_").replace("\\s^","\\sigma^")
+                      dict0[par] = "\\big("+ names[texpar].replace("\\e","\\epsilon").replace("=","\\big)_{"+par[len(par)-2:]+"} = \\Big(")+"\\Big)_{"+par[len(par)-2:]+"}".replace("dag","dagger").replace("\\s_","\\sigma_").replace("\\s^","\\sigma^")
+                      found = True
+                      print("{0} ----> {1}".format(par, dict0[par]))
+               elif texpar == par.replace("Re","").replace("Im",""):
+                   par = par.replace("Re","").replace("Im","")
+                   dict0[par] = names[texpar].replace("\\e","\\epsilon")
+                   found = True
+                   print("{0} ----> {1}".format(par, dict0[par]))
+
+            if found == False: print("Not found "+par)
+        
+        with open("out_"+case+".json","w") as f:
+             json.dump(dict0,f,indent=4)
